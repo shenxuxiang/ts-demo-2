ee851df1068bea0d7a3a7a6b997b44444feebdee
shenxuxiang <shenxuxiang@shenxuxiangdeMacBook-Pro.local>
2020-10-04 16:29:41 +0800

    feat: 修改了内容

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterComp from './RouterComp';
 
-// 这里就是改变
+// 这里就是改变xxxxxxx
 ReactDOM.render(<RouterComp />, document.getElementById('root'));
