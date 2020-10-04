d493f2a2264d592de63cfa9fce008d674f1d28ab
shenxuxiang <shenxuxiang@shenxuxiangdeMacBook-Pro.local>
2020-10-04 19:11:56 +0800

    chore: 修改了内容

--- src/pages/Goods/index.tsx
+++ src/pages/Goods/index.tsx
@@ -11,7 +11,7 @@ interface IState {
 export default class Goods extends Component<IProps, IState> {
   public render() {
     return (
-      <div>goods hahahah</div>
+      <div>goods123456</div>
     );
   }
 }
